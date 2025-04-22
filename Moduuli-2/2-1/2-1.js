a209df9b740a8b26248daf4716af70865857fcff
rennel <renne.lehto@metropolia.fi>
2025-04-22 13:41:30 +0300

    2-1

---
+++ Moduuli-2/2-1/2-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const luvut = [];
+
+luvut[0] = prompt('Anna ensimmäinen luku')
+luvut[1] = prompt('Anna toinen luku')
+luvut[2] = prompt('Anna kolmas luku')
+luvut[3] = prompt('Anna neljäs luku')
+luvut[4] = prompt('Anna viides luku')
+
+for (let i = 4; i > -1; i--) {
+        console.log(luvut[i])
+    }
