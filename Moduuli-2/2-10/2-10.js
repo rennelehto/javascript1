8bd80d82c9e19e4a047949c177ae695156d09463
rennel <renne.lehto@metropolia.fi>
2025-04-26 18:32:19 +0300

    2-10

---
+++ Moduuli-2/2-10/2-10.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const ehdolla = +prompt(`Syötä ehdokkaiden lukumäärä:`)
+const ehdokkaat =[]
+for (let e = 0; e < ehdolla; e++){
+  const ehd = prompt(`Syötä ${e+1}. ehdokkaan nimi:`)
+  ehdokkaat[e] = {
+    nimi:ehd,
+    nnet:0,
+  }
+}
+const voters = +prompt(`Syötä äänestäjien lukumäärä:`)
+
+for (let a = 1; a <= voters; a++){
+  const ni = prompt(`Kirjoita ehdokkaasi nimi:`)
+  for (let y = 0; y < ehdokkaat.length; y++){
+    if (ehdokkaat[y].nimi === ni){
+      ehdokkaat[y].nnet += 1;
+    }
+  }
+}
+
+ehdokkaat.sort((a, b) => b.nnet - a.nnet)
+
+document.querySelector('#tulos').innerHTML = `Äänestyksen voittaja on ${ehdokkaat[0].nimi}, saatuaan ääniä ${ehdokkaat[0].nnet} kpl.`;
